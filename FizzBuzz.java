58eae6ad6ab557abcdf21ebb93cb058574fd419f
Uriy <ru.spam34@gmail.com>
2016-08-01 22:00:39 +0300

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,8 +3,9 @@ package test;
 public class FizzBuzz { 
  
  public static void main(String[] args) {
+ 	String result;
 	for (int i=1;i<=100;i++) {
-	 String result="";
+	 result="";
 	 if (i%3==0) result+="Fizz"; 
 	 if (i%5==0) result+="Buzz";
  	 if (result.isEmpty()) result = ""+i;
