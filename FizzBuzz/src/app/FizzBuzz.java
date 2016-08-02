750cd5c5e52d3cc24e3892fd9aa0e798ef562fed
Master <>
2016-08-03 00:11:33 +0300

    MinorUpdates.

---
+++ FizzBuzz/src/app/FizzBuzz.java
@@ -0,0 +1,16 @@
+package app;
+ 
+public class FizzBuzz { 
+ 
+ public static void main(String[] args) {
+ 	String result;
+	for (int i=1;i<=100;i++) {
+	 result="";
+	 if (i%3==0) result+="Fizz"; 
+	 if (i%5==0) result+="Buzz";
+ 	 if (result.isEmpty()) result = ""+i;
+	 System.out.println(result);
+	}  
+ }
+ 
+}
