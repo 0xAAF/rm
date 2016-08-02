80f94cbbe5bc201b8721afcfa6dec9d465a5a5a1
Master <>
2016-08-02 23:52:49 +0300

    MinorUpdates. add full java project

---
+++ CalculatorV2/src/ActionList.java
@@ -0,0 +1,16 @@
+ 
+package javacalculator;
+
+import java.util.ArrayList;
+ 
+public class ActionList  extends ArrayList<Action>{
+ 
+	public int indexOf(String act) { 
+		for (int i=0; i<this.size();i++) {
+			if (get(i).getName().equals(act)) {
+				return i;
+			}
+		}
+    return -1;
+	}
+}
---
+++ CalculatorV2/src/Validator.java
@@ -0,0 +1,12 @@
+package javacalculator;
+ 
+import java.util.ArrayList;
+
+public class Validator extends ArrayList<String>{
+ 
+ public boolean isValid(String text) { 
+	 for (String match:this)
+		 if (!text.matches(match)) return false;
+	 return true;
+ }
+}
