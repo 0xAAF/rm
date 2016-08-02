607159d89a1cabd63cada864adf062fa2cb47698
Uriy <ru.spam34@gmail.com>
2016-08-02 23:40:50 +0300

    Add files via upload

---
+++ javaCalculatorV2/Action.java
@@ -0,0 +1,20 @@
+package javacalculator;
+
+import java.util.ArrayList;
+
+public abstract class Action {
+	private final String name;
+	Action(String name) {
+		this.name = name;
+	}
+	public float calculate(ArrayList<Float> value) { 
+		float result = value.get(0);
+		for (int i = 1; i<value.size();i++)
+			result=action(result,value.get(i));
+		return result;
+	}
+	public String getName() {
+		return name;
+	}
+	abstract public float action(float a, float b);
+}
---
+++ javaCalculatorV2/Calculator.java
@@ -0,0 +1,31 @@
+package javacalculator;
+
+class Calculator {
+	private final Inputer input;
+	private final ActionList actions;
+	private float result;
+	Calculator (ActionList actions, Validator validator, String delimeter) {
+		this.actions=actions;
+		this.input=new Inputer(validator,delimeter);
+	}
+	public boolean update() {
+		input.readln();
+		int actionNumber = actions.indexOf(input.getAction());
+		if (actionNumber>=0) 
+			result = actions.get(actionNumber).calculate(input.getValues());
+		else return false;
+		return true;
+	}
+	public String getAction() {
+		return input.getAction();
+	}
+	public String getString() {
+		return input.getInputPrint();
+	}
+	public float getResult() {
+		return result;
+	}
+	public String getInput() {
+		return input.getInput();
+	}
+}
